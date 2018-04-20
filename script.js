a46bb6c4fd06f222a034493f23e89b280fb0b87e
Delchev <delchev@abv.bg>
2018-04-20 15:31:18 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function change(){
 	document.getElementById("pic").setAttribute("src", "pics/change.jpg");
 }
+
+$.document.ready(function(){
+	fetchJSON();
+});
+
+function fetchJSON(){
+	let newData;
+	$.get("http://jsonplaceholder.typicode.com/photos", function(data){
+		newData = data;
+		var jObj = JSON.parse(data.)
+		console.log(JSON.parse(data));
+	});
+}
